0a604490843b67f1ab66791657ce39deb646b32b
vladimir-m88 <mirniy.v.v@gmail.com>
2016-07-10 01:23:28 +0300

    Add project files.

---
+++ GitTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTest1.Startup))]
+namespace GitTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
